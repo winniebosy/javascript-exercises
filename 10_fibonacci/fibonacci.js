86fb662d86fe91f9218d62811d8db067a6687043
Winnie Bosibori <winniebosy@gmail.com>
2022-08-04 18:07:35 +0300

    fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < count; i++) {
+        let nextSequence = b
+        b = a + b;
+        a = nextSequence
+    }
+    return b
 
 };
 
