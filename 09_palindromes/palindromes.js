f8b1c2440731bd0e027a4d9d6c4e11ef7f28b75d
Winnie Bosibori <winniebosy@gmail.com>
2022-08-04 14:09:12 +0300

    palindrome solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function(str) {
+    //remove unwanted characters
+    var re = /[\W_]/g;
+    let splitA = str.toLowerCase().replace(re, '');
+    //reverse and compare
+    let reverses = splitA.split('').reverse().join('');
+    if (splitA === reverses) {
+        console.log('Is a palindorme')
+    } else {
+        console.log('not a palindrom');
+    }
+    return splitA === reverses
+
 
 };
 
